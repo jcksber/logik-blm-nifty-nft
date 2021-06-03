d087cc985fbc27067fa56b8f39b2c1deda06aaa0
Jack Kasbeer <kasbeer@cmu.edu>
2021-06-03 15:19:05 -0500

    added hardhat-ethers and set up to work w alchemy & rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { STAGING_ALCHEMY_API_URL,
+		STAGING_PRIVATE_KEY, 
+		STAGING_PUBLIC_KEY,
+		MAINNET_ALCHEMY_API_URL,
+		MAINNET_PRIVATE_KEY,
+		MAINNET_PUBLIC_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.8.0",
+	solidity: "0.8.0",
+	defaultNetwork: "rinkeby",
+	networks: {
+		hardhat: {},
+		rinkeby: {
+			url: STAGING_ALCHEMY_API_URL,
+			accounts: [`0x${STAGING_PRIVATE_KEY}`]
+		}
+	}
 };
